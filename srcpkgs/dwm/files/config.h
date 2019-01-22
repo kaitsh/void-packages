62f089728bd42a7147b3baa147ca0fdde043ebd0
kaitsh <kaitsh@d-git.de>
2019-01-22 14:47:35 -0300

    dwm: fix scratpad keybinding

--- srcpkgs/dwm/files/config.h
+++ srcpkgs/dwm/files/config.h
@@ -87,7 +87,6 @@ static Key keys[] = {
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_minus,  togglescratch,  {.v = scratchpadcmd } },
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
