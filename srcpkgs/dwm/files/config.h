4b10a72412d1d56c264c2c6b387fbf74c08cdbed
kaitsh <kaitsh@d-git.de>
2019-01-22 14:16:15 -0300

    dwm: adjust keybindings

--- srcpkgs/dwm/files/config.h
+++ srcpkgs/dwm/files/config.h
@@ -1,6 +1,5 @@
 /* See LICENSE file for copyright and license details. */
 
-<<<<<<< HEAD
 /* extra keys */
 #include <X11/XF86keysym.h>
 
@@ -15,15 +14,11 @@ static const char *mute[] = { "amixer", "-q", "set", "Master", "toggle", NULL };
 static const char *upbright[] = { "xbacklight", "-inc", "2", NULL NULL };
 static const char *downbright[] = { "xbacklight", "-dec", "2", NULL, NULL };
 
-
-=======
->>>>>>> dbc1016955... dwm: add basic configuration
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-<<<<<<< HEAD
 static const char *fonts[]          = {"Source Code Pro:style=Regular:size=9", "SauceCodePro NF:style=Regular:size=14"};
 static const char dmenufont[]       = "Source Code Pro:style=Regular:size=9";
 static const char col_gray1[]       = "#1c2023";
@@ -34,27 +29,11 @@ static const char col_cyan[]        = "#95aec7";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray1 },
-=======
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
->>>>>>> dbc1016955... dwm: add basic configuration
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 
 /* tagging */
-<<<<<<< HEAD
 static const char *tags[] = { "\ue7a2", "\ue796", "\uf269", "\uf1de", "\uf6ed", "\uf8b2", "\uf8b5", "\uf8b8", "\uf8bb" };
-=======
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
->>>>>>> dbc1016955... dwm: add basic configuration
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -62,26 +41,16 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class                       instance    title       tags mask     isfloating   monitor */
-<<<<<<< HEAD
 	{ "Firefox",                   NULL,       NULL,       1 << 2,       1,           -1 },
 	{ "Thunderbird",               NULL,       NULL,       1 << 4,       1,           -1 },
 	{ "Emacs",                     NULL,       NULL,       1 << 1,       0,           -1 },
-=======
-	{ "Gimp",                      NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox Developer Edition", NULL,       NULL,       1 << 3,       1,           -1 },
-	{ "Thunderbird",               NULL,       NULL,       1 << 6,       1,           -1 },
->>>>>>> dbc1016955... dwm: add basic configuration
 };
 
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
-<<<<<<< HEAD
 static const unsigned int gappx     = 2;        /* gap pixel between windows */
-=======
-static const unsigned int gappx     = 6;        /* gap pixel between windows */
->>>>>>> dbc1016955... dwm: add basic configuration
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
@@ -110,7 +79,6 @@ static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-<<<<<<< HEAD
 	{ 0,             XF86XK_MonBrightnessUp,   spawn,          {.v = upbright } },
 	{ 0,             XF86XK_MonBrightnessDown, spawn,          {.v = downbright } },
 	{ 0,             XF86XK_AudioRaiseVolume,  spawn,          {.v = upvol } },
@@ -119,11 +87,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_minus,  togglescratch,  {.v = scratchpadcmd } },
-=======
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } },
->>>>>>> dbc1016955... dwm: add basic configuration
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -131,15 +95,9 @@ static Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-<<<<<<< HEAD
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
-=======
-	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
->>>>>>> dbc1016955... dwm: add basic configuration
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
@@ -160,11 +118,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-<<<<<<< HEAD
 	{ MODKEY|ShiftMask,             XK_e,      quit,           {0} },
-=======
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
->>>>>>> dbc1016955... dwm: add basic configuration
 };
 
 /* button definitions */
